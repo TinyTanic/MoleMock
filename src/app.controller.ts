35cc7a5a9452e13fe558f8c5f636836d04522795
Luca Aguzzoli <l.aguzzoli@molecle.com>
2018-10-19 18:40:49 +0200

    Added jsoneditor to route page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,36 +1,46 @@
 import { Get, Controller, Render, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { WorkspacesService } from 'workspaces/workspaces.service';
+import { RoutesService } from './routes/routes.service';
 
 @Controller()
 export class AppController {
 
   constructor(
     private readonly _workspacesService: WorkspacesService,
+    private readonly _routesService: RoutesService,
   ) {}
 
   @Get()
   @Render('index')
   public root() {
-    return {
-      message: 'hello world!'
-    }
+    return ({
+      message: 'hello world!',
+    });
     // return this.appService.root();
   }
 
   @Get('workspaces')
   @Render('workspaces')
   public async workspaces() {
-    return {
-      workspaces: await this._workspacesService.getAll()
-    }
+    return ({
+      workspaces: await this._workspacesService.getAll(),
+    });
   }
 
   @Get('workspace/:id')
-  @Render('workspace')
+  @Render('workspace-detail')
   public async workspace(@Param() params) {
-    return {
-      workspace: await this._workspacesService.getById(params.id)
-    }
+    return ({
+      workspace: await this._workspacesService.getById(params.id),
+    });
+  }
+
+  @Get('route/:id')
+  @Render('route-detail')
+  public async route(@Param() params) {
+    return ({
+      route: await this._routesService.getById(params.id),
+    });
   }
 }
