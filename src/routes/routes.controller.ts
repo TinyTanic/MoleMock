563dd68384888fec13ca2b0ecb443dfd76f9f573
Luca Aguzzoli <luca.aguzzoli@spindox.it>
2019-08-08 17:54:08 +0200

    add user auth in route model

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -15,6 +15,7 @@ import { CreateRouteDto, PatchRouteDto } from './models/route.dto';
 import { Route } from './models/route.entity';
 import { IRoute } from './models/route.interface';
 import { RoutesService } from './routes.service';
+import { User } from '../common/decorators/user.decorator';
 
 @Controller('api/route')
 @UseGuards(JwtAuthGuard)
@@ -22,28 +23,35 @@ export class RoutesController {
   constructor(private readonly _routesService: RoutesService) {}
 
   @Get()
-  public getAll(): Promise<Route[]> {
-    return this._routesService.getAll();
+  public getAll(@User() user): Promise<Route[]> {
+    return this._routesService.getAll(user);
   }
 
   @Get(':id')
-  public getById(@Param() params): Promise<Route> {
-    return this._routesService.getById(params.id);
+  public getById(@Param() params, @User() user): Promise<Route> {
+    return this._routesService.getById(params.id, user);
   }
 
   @Post()
-  public async create(@Body() createRouteDto: CreateRouteDto): Promise<IRoute> {
-    return await this._routesService.create(createRouteDto);
+  public async create(
+    @Body() createRouteDto: CreateRouteDto,
+    @User() user,
+  ): Promise<IRoute> {
+    return await this._routesService.create(createRouteDto, user);
   }
 
-  @Patch()
-  public async update(@Body() route: PatchRouteDto) {
-    return this._routesService.update(route);
+  @Patch(':id')
+  public async update(
+    @Body() route: PatchRouteDto,
+    @Param('id') routeId,
+    @User() user,
+  ) {
+    return this._routesService.update(routeId, route, user);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  public async remove(@Param() params) {
-    await this._routesService.remove(params.id);
+  public async remove(@Param() params, @User() user) {
+    await this._routesService.remove(params.id, user);
   }
 }
--- src/user/dto/user.entity.ts
+++ src/user/dto/user.entity.ts
@@ -1,23 +1,31 @@
 import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 
 import { Workspace } from '../../workspaces/models/workspace.entity';
+import { Route } from '../../routes/models/route.entity';
 
 @Entity()
 export class User {
+  @PrimaryColumn()
+  id: string;
 
-  @PrimaryColumn() id: string;
+  @Column()
+  email: string;
 
-  @Column() email: string;
+  @Column()
+  username: string;
 
-  @Column() username: string;
+  @Column()
+  password: string;
 
-  @Column() password: string;
+  @Column()
+  name: string;
 
-  @Column() name: string;
-
-  @Column() surname: string;
+  @Column()
+  surname: string;
 
   @OneToMany(type => Workspace, workspace => workspace.user)
   workspaces: Workspace[];
 
+  @OneToMany(type => Route, route => route.user)
+  routes: Route[];
 }
